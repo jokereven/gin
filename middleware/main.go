e33d02ca5f5d14833f911e9710395c818cdc8b21
jokereven <zhou125disorder@163.com>
2021-11-06 21:26:42 +0800

    middleware

--- middleware/main.go
+++ middleware/main.go
@@ -9,17 +9,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func m(c *gin.Context) {
-	fmt.Println("this is a middleware")
-	start := time.Now()
-	c.Set("name", "小王子") // 可以通过c.Set在请求上下文中设置值，后续的处理函数能够取到该值
-	// 调用该请求的剩余处理程序
-	c.Next()
-	// 不调用该请求的剩余处理程序
-	// c.Abort()
-	// 计算耗时
-	cost := time.Since(start)
-	log.Println(cost)
+// StatCost 是一个统计耗时请求耗时的中间件
+func StatCost() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Set("name", "小王子") // 可以通过c.Set在请求上下文中设置值，后续的处理函数能够取到该值
+		// 调用该请求的剩余处理程序
+		c.Next()
+		// 不调用该请求的剩余处理程序
+		// c.Abort()
+		// 计算耗时
+		cost := time.Since(start)
+		log.Println(cost)
+	}
 }
 
 func indexHander(c *gin.Context) {
@@ -31,12 +33,36 @@ func indexHander(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
+	// 路由组 添加中间件
+	// userGroup := r.Group("/user")
+	// 方法一
+	// userGroup := r.Group("/user", StatCost())
+
+	// 方法二
+	userGroup := r.Group("/user")
+	userGroup.Use(StatCost())
+	{
+		userGroup.GET("/index", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"massage": "ok/index",
+			})
+		})
+		userGroup.GET("/code520", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"massage": "ok/code520",
+			})
+		})
+	}
 	// 全局注册中间件
-	r.Use(m)
+	r.Use(StatCost())
 	r.GET("/index", indexHander)
 	r.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"massage": "test"})
+	})
+	r.GET("/onlyone", StatCost(), func(c *gin.Context) {
+		fmt.Println("this is a onlyone router")
 		c.JSON(http.StatusOK, gin.H{
-			"massage": "test",
+			"massage": "this is onlyone router",
 		})
 	})
 	r.Run(":8080")
